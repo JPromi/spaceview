0a5669e75720d6645b9252ce1ec6b0c22c873929
JPromi <jonasmpr05@gmail.com>
2026-08-24 08:58:37 +0200

    style title

--- shared/src/commonMain/kotlin/com/jpromi/spaceview/elements/forms/SettingsSection.kt
+++ shared/src/commonMain/kotlin/com/jpromi/spaceview/elements/forms/SettingsSection.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jpromi.spaceview.AppTheme
 
 @Composable
@@ -35,6 +36,7 @@ fun SettingsSection(
             text = title,
             color = AppTheme.textColor,
             fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp,
         )
 
         content()
