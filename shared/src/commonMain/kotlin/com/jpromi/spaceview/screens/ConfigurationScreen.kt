dd13594c6f9268800bd0c04da8323ac6b2e486f7
JPromi <jonasmpr05@gmail.com>
2026-09-07 10:32:57 +0200

    Add safedrawing to main pages

--- shared/src/commonMain/kotlin/com/jpromi/spaceview/screens/ConfigurationScreen.kt
+++ shared/src/commonMain/kotlin/com/jpromi/spaceview/screens/ConfigurationScreen.kt
@@ -14,12 +14,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
@@ -310,7 +313,8 @@ fun ConfigurationScreen(
     Row(
         modifier = Modifier
             .fillMaxSize()
-            .background(AppTheme.background),
+            .background(AppTheme.background)
+            .windowInsetsPadding(WindowInsets.safeDrawing),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
         // Sidebar
--- shared/src/commonMain/kotlin/com/jpromi/spaceview/screens/RoomScreen.kt
+++ shared/src/commonMain/kotlin/com/jpromi/spaceview/screens/RoomScreen.kt
@@ -10,13 +10,16 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -170,6 +173,7 @@ fun RoomScreen(
             .fillMaxSize()
             .background(color = AppTheme.background)
             .padding(32.dp)
+            .windowInsetsPadding(WindowInsets.safeDrawing)
     ) {
         if (isLoadingRoom || isLoadingAvailability) {
             // loading
